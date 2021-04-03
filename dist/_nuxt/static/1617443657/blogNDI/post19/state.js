e54d9901b9466c37567a411e365b64cec62bf2aa
francesco66 <>
2021-04-03 15:47:32 +0200

    pulizia

--- dist/_nuxt/static/1617443657/blogNDI/post19/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnonfacciamoneundramma\u002F_nuxt\u002Fstatic\u002F1617443657",layout:"default",error:a,serverRendered:true,routePath:"\u002FblogNDI\u002Fpost19",config:{app:{basePath:"\u002Fnonfacciamoneundramma\u002F",assetsPath:"\u002Fnonfacciamoneundramma\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7d93a34e"}}}}(null));
--- dist/_nuxt/static/1617443657/dovesiamo/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/dovesiamo", {data:[{}],fetch:{},mutations:void 0});
