edbae8c5b728d7b4984836de230f4ddcd6ef7be3
francesco66 <francescoarmandoporta@gmail.com>
2021-06-05 08:52:50 +0200

    primo restyling

--- pages/blogMDMA/_slug.vue
+++ pages/blogMDMA/_slug.vue
@@ -1,25 +1,29 @@
 <template>
-  <article class="pa3 pa5-ns">
-    <nav class="flex justify-between bb b--white-10">
-      <NuxtLink class="f6 link dim br3 ba bw1 ph3 pv2 mb3 ml3 black" to="/HomeMDMA">Mai Dire Mi Arrendo</NuxtLink>
-      <!-- <div class="flex-grow pa3 flex items-center">
-        <AppSearchInputMDMA class="f6 link dib white dim mr3 mr4-ns"/>
-      </div> -->
+  <article class="pa3 pa3-ns pl5-ns">
+    <nav class="flex justify-between bb bg-purple w-90 h1 ph3 pv3 pv4-ns ph4-m ph5-l">
+      <div class="flex-grow br3 bw1 pa2 pl1 flex items-center">
+      	<PrevNextMDMA :prev="prev" />
+      	<PrevNextMDMA :next="next" />
+      </div>
+      <div class="flex-grow pa2 flex items-center">
+        <NuxtLink class="link grow br3 ba bw1 pa2 mb3 bg-animate hover-bg-near-black bg-light-purple white" to="/HomeMDMA">MDMA Home</NuxtLink>
+      </div>
+      <div class="flex-grow pa2 flex items-center">
+        <a class="link grow br3 ba bw1 pa2 mb3 ml3 bg-animate hover-bg-light-purple bg-black-50 white" v-on:click="onClick(article)">Scarica</a>
+        <a class="link grow br3 ba bw1 pa2 mb3 ml3 bg-animate hover-bg-light-purple bg-black-50 white" v-on:click="onClick(article)">Commenta</a>
+      </div>
     </nav>
 
-    <h2 class="f3 f1-l f-headline-l ma2 mb3">{{ article.titolo }}</h2>
-    <h2 class="measure lh-copy dib ma2 mb5">di {{ article.autore }}</h2>
-    <span class="measure lh-copy ma6">{{ article.data }}</span>
-    <!-- <p v-if="article.tema" class="measure lh-copy ma3">Tema: {{ article.tema }}</p> -->
-    <!-- <a class="f6 link dim br3 ba ph3 pv2 mb2 dib near-black" v-on:click="show_parole= !show_parole">Vedi parole</a>
-    <p v-if="show_parole" class="measure lh-copy di">  {{article.parole }}</p> -->
-    <!-- <div> -->
-      <embed v-if="article.video"
-        type="video/webm"
-        :src="article.video"
-        width="250"
-        height="200">
-    <!-- </div> -->
+    <h2 class="f3 f1-l f-headline-l mt4 ma2 mb3">{{ article.titolo }}</h2>
+    <h2 class="measure lh-copy dib ma2 mb5">{{ article.autore }}</h2>
+    <span class="measure lh-copy ma5" v-if="article.data">{{ article.data }}</span>
+
+    <embed v-if="article.video"
+      type="video/webm"
+      :src="article.video"
+      width="250"
+      height="200">
+
     <p class="measure lh-copy mb4">
       <nuxt-content :document="article" />
     </p>
@@ -37,11 +41,12 @@
 </template>
 
 <script>
+import { saveAs } from 'file-saver';
+
 export default {
   async asyncData({ $content, params }) {
     const article = await $content('articlesMDMA', params.slug, { 'text': true }).fetch()
-    let show_parole = false
-
+    
     const [prev, next] = await $content('articlesMDMA')
         /*.only(['title', 'slug', 'autore', 'data'])*/
         /* .sortBy('createdAt', 'asc') */
@@ -53,9 +58,40 @@ export default {
       article,
       prev,
       next,
-      show_parole
     }
+  },
+
+  methods: {
+    // per scaricare il file in formato testo
+    async onClick(article) {
+      var blob = new Blob(["\n" + article.autore + "\n\n" + article.titolo + "\n\n" + article.text], {type: "text/plain;charset=utf-8"});
+      saveAs(blob, article.slug + ".txt");
+    },
+    // per mandare una mail con correzioni 
+    async onSend(article) {
+      var blob = new Blob(["\n" + article.autore + "\n\n" + article.titolo + "\n\n" + article.text], {type: "text/plain;charset=utf-8"});
+      //saveAs(blob, article.slug + ".txt");
+    }
+
+
   }
+
+/*
+      axios({
+        url: path, //'http://localhost:8000/my.pdf',
+        method: 'GET',
+        responseType: 'blob',
+      }).then((response) => {
+        var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+        var fileLink = document.createElement('a');
+
+        fileLink.href = fileURL;
+        fileLink.setAttribute('download', 'file.pdf');
+        document.body.appendChild(fileLink);
+        fileLink.click();
+        });
+        */
+
 }
 </script>
 
--- pages/blogNDI/_slug.vue
+++ pages/blogNDI/_slug.vue
@@ -1,25 +1,29 @@
 <template>
-  <article class="pa3 pa5-ns">
-    <nav class="flex justify-between bb b--white-10">
-      <NuxtLink class="f6 link dim br3 ba bw1 ph3 pv2 mb3 ml3 black" to="/HomeMDMA">Nel Dominio Dell'Incerto</NuxtLink>
-      <!-- <div class="flex-grow pa3 flex items-center">
-        <AppSearchInputNDI class="f6 link dib white dim mr3 mr4-ns"/>
-      </div> -->
+  <article class="pa3 pa3-ns pl5-ns">
+    <nav class="flex justify-between bb bg-purple w-90 h1 ph3 pv3 pv4-ns ph4-m ph5-l">
+      <div class="flex-grow br3 bw1 pa2 pl1 flex items-center">
+      	<PrevNextNDI :prev="prev" />
+      	<PrevNextNDI :next="next" />
+      </div>
+      <div class="flex-grow pa2 flex items-center">
+        <NuxtLink class="link grow br3 ba bw1 pa2 mb3 bg-animate hover-bg-near-black bg-light-purple white" to="/HomeNDI">Nel Dominio dell'Incerto Home</NuxtLink>
+      </div>
+      <div class="flex-grow pa2 flex items-center">
+        <a class="link grow br3 ba bw1 pa2 mb3 ml3 bg-animate hover-bg-light-purple bg-black-50 white" v-on:click="onClick(article)">Scarica</a>
+        <a class="link grow br3 ba bw1 pa2 mb3 ml3 bg-animate hover-bg-light-purple bg-black-50 white" v-on:click="onClick(article)">Commenta</a>
+      </div>
     </nav>
 
-    <h2 class="f3 f1-l f-headline-l ma2 mb3">{{ article.titolo }}</h2>
-    <h2 class="measure lh-copy dib ma2 mb5">di {{ article.autore }}</h2>
-    <span class="measure lh-copy ma6">{{ article.data }}</span>
-    <!-- <p v-if="article.tema" class="measure lh-copy ma3">Tema: {{ article.tema }}</p> -->
-    <!-- <a class="f6 link dim br3 ba ph3 pv2 mb2 dib near-black" v-on:click="show_parole= !show_parole">Vedi parole</a>
-    <p v-if="show_parole" class="measure lh-copy di">  {{article.parole }}</p> -->
-    <!-- <div> -->
-      <embed v-if="article.video"
-        type="video/webm"
-        :src="article.video"
-        width="250"
-        height="200">
-    <!-- </div> -->
+    <h2 class="f3 f1-l f-headline-l mt4 ma2 mb3">{{ article.titolo }}</h2>
+    <h2 class="measure lh-copy dib ma2 mb5">{{ article.autore }}</h2>
+    <span class="measure lh-copy ma5" v-if="article.data">{{ article.data }}</span>
+
+    <embed v-if="article.video"
+      type="video/webm"
+      :src="article.video"
+      width="250"
+      height="200">
+
     <p class="measure lh-copy mb4">
       <nuxt-content :document="article" />
     </p>
