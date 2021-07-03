73c11556e94bab9f8c0783af0813fded49faf94d
francesco66 <francescoarmandoporta@gmail.com>
2021-07-03 15:37:38 +0200

    mail is working?

--- node_modules/.cache/nuxt/index.js
+++ node_modules/.cache/nuxt/index.js
@@ -60,7 +60,7 @@ async function createApp(ssrContext, config = {}) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"title":"nonfacciamoneundramma","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"script":[{"hid":"MailtoUI","src":"js\u002Fmailtoui-min.js","defer":true}],"style":[]},
+    head: {"title":"nonfacciamoneundramma","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"script":[{"hid":"MailtoUI","src":"mailtoui-min.js","defer":true}],"style":[]},
 
     router,
     nuxt: {
