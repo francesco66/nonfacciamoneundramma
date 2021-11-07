4fa9d4f21cc97429c8a93c8ce6cdca0e40cdb3fc
francesco66 <francescoarmandoporta@gmail.com>
2021-11-07 09:21:03 +0100

    fa46e88a

---
+++ _nuxt/static/1636273250/blogMDMA/Diego-Abha/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnonfacciamoneundramma\u002F_nuxt\u002Fstatic\u002F1636273250",layout:"default",error:a,serverRendered:true,routePath:"\u002FblogMDMA\u002FDiego-Abha",config:{app:{basePath:"\u002Fnonfacciamoneundramma\u002F",assetsPath:"\u002Fnonfacciamoneundramma\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f834041e"}}}}(null));
